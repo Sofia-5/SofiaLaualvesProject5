88d54037c189c8369a190d38f92dbbf1ef6be29f
Sofia <sofia.lau.alves@gmail.com>
2020-08-10 16:42:49 -0400

    Updates

---
+++ precache-manifest.65eb2bcb15a284992842e025dbb21179.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d53ddcd2c41620e18855e13292889246",
+    "url": "/SofiaLaualvesProject5/index.html"
+  },
+  {
+    "revision": "f78bcd5c7bcc4c3b49c4",
+    "url": "/SofiaLaualvesProject5/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "531e1a6aa8ac5e91ed4e",
+    "url": "/SofiaLaualvesProject5/static/js/2.43772cbd.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/SofiaLaualvesProject5/static/js/2.43772cbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f78bcd5c7bcc4c3b49c4",
+    "url": "/SofiaLaualvesProject5/static/js/main.bdbc6d3c.chunk.js"
+  },
+  {
+    "revision": "d1a13b42698fa144a504",
+    "url": "/SofiaLaualvesProject5/static/js/runtime-main.82e103d9.js"
+  }
+]);
