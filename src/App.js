0430e1fba5d8c878189d6142e079cffca9ab39d1
Sofia <sofia.lau.alves@gmail.com>
2020-08-12 20:29:28 -0400

    fixed SVG clickable image

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import firebase from './firebase';
+import House from './House';
 import './App.css';
 
-// Listen for a click event when the user clicks on the image part, using onClick method 
-// filter() through my array in Firebase to find the correct/matching property/key of the property/key associated with the user's chosen image part
-// Print the property/key and value from Firebase to the page, using .map()
+// Listen for a click event when the user clicks on the image part (button), using onClick method 
+// Grab specific ID in the object in Firebase to find the correct/matching key of the key associated with the user's chosen image part (button)
+// Print the key and value from Firebase to the page ternary operator 
 
 // What do I need? 
-  // SVG image with unique property/key names 
+  // SVG image with unique key names 
   // Some state!
   // componentDidMount
+  // handeClick
 
   // Home Feature Clicked = selectedHomeFeature
-  // Sustainable Alternatives = option
+  // Sustainable Alternatives = selectedOption
   
   
 
@@ -20,53 +22,72 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      selectedHomeFeature: [],
+      selectedHomeFeature: "",
       data: {},
-      selectedOption: {}
+      selectedOption: {},
+      nextOption: []
     }
   }
 
   componentDidMount() { 
     const dbRef = firebase.database().ref();
     dbRef.on('value', (snapshot) => {
-      const data = snapshot.val();
-      console.log( data );
-      
+          
       this.setState({
-        data,
+        data: snapshot.val(),
       })
     })
   }
   // ask teacher about loading states 
 
-  handleClick = (event) => {
-    console.log(event.target.id);
-    const category = this.state.data[event.target.id]
-    const selectedOptionTitle = Object.keys(category)[0]
-    const selectedOption = category[Object.keys(category)[0]]
+  handleClick = (id) => {
+    console.log(id);
+ 
+    // const category = this.state.data[event.target.id]
+    const category = this.state.data[id]
   
     this.setState({
-      selectedOption, selectedOptionTitle,
+      selectedOption: category[Object.keys(category)[0]],
+      selectedHomeFeature: Object.keys(category)[0]
+
     })
   }
+
+  // incrementCounter = (nextObj) => {
+  //   const goToNextObject = this.state.data[id] 
+  //   this.setState({
+  //     nextIndex 
+  //   })
+  // }
 
   render () {
     return (
 
-      <div className="App">
+      <div className="App wrapper">
         <h1>A Sustainable Home</h1>
-        <button id="Roof" onClick={this.handleClick}>Roof</button>
+
+
+        <House handleClickProp={this.handleClick}/>
+        
+
+        {/* <button id="Roof" onClick={this.handleClick}>Roof</button>
+
+        <button id="Windows" onClick={this.handleClick}>Windows</button>
+
+        <button id="Door" onClick={this.handleClick}>Door</button> */}
 
         { 
-        this.state.selectedOption? 
+        this.state.selectedHomeFeature !== "" ? 
         <div className="displayContainer"> 
           <div className="content">
-            <h2>{this.state.selectedOptionTitle}</h2>
-            <p> {this.state.selectedOption.Description}</p>
+            <h2>{this.state.selectedHomeFeature}</h2>
+            <p>{this.state.selectedOption.Description}</p>
           </div>
-          <img src={this.state.selectedOption.Image}/> 
+          <div className="imgContainer">
+            <img src={this.state.selectedOption.Image}/> 
+          </div>
         </div> 
-        : null  
+        : <p className="instructions">Click on a part of the house or trees above to learn about sustainable features to help save you on your energy bill and help improve the health of our planet.</p>  
         }
       </div>
     );
